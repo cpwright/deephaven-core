0770dc033af1a1f7b4555cdcba381e11dd89e9f6
Charles P. Wright <charleswright@deephaven.io>
2026-09-23 09:24:55 -0400

    fix: DH-23738: Build both-refreshing as-of join left indexes with the left node size

--- engine/table/src/main/java/io/deephaven/engine/table/impl/AsOfJoinHelper.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/AsOfJoinHelper.java
@@ -942,7 +942,9 @@ public class AsOfJoinHelper {
         final boolean reverse = order == SortingOrder.Descending;
 
         final ChunkType stampChunkType = rightStampSource.getChunkType();
-        final Supplier<SegmentedSortedArray> ssaFactory =
+        final Supplier<SegmentedSortedArray> leftSsaSupplier =
+                SegmentedSortedArray.makeFactory(stampChunkType, reverse, control.leftSsaNodeSize());
+        final Supplier<SegmentedSortedArray> rightSsaSupplier =
                 SegmentedSortedArray.makeFactory(stampChunkType, reverse, control.rightSsaNodeSize());
         final SsaSsaStamp ssaSsaStamp = SsaSsaStamp.make(stampChunkType, reverse);
 
@@ -1019,7 +1021,7 @@ public class AsOfJoinHelper {
 
             @Override
             public SegmentedSortedArray apply(RowSet rightRowSet) {
-                final SegmentedSortedArray ssa = ssaFactory.get();
+                final SegmentedSortedArray ssa = rightSsaSupplier.get();
                 final int slotSize = rightRowSet.intSize();
                 if (slotSize > 0) {
                     rightRowSet.fillRowKeyChunk(rightStampKeys.ensureCapacity(slotSize));
@@ -1040,7 +1042,7 @@ public class AsOfJoinHelper {
 
             @Override
             public SegmentedSortedArray apply(RowSet leftRowSet) {
-                final SegmentedSortedArray ssa = ssaFactory.get();
+                final SegmentedSortedArray ssa = leftSsaSupplier.get();
                 final int slotSize = leftRowSet.intSize();
                 if (slotSize > 0) {
 
